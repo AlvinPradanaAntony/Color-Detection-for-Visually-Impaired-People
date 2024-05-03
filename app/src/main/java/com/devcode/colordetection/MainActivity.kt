540a84d26c5a71875715333ade09e14fa0de9bd9
AlvinPradanaAntony <quickgaming321@gmail.com>
2024-05-03 23:42:43 +0700

    Add OpenCV Camera

--- app/src/main/java/com/devcode/colordetection/MainActivity.kt
+++ app/src/main/java/com/devcode/colordetection/MainActivity.kt
@@ -1,18 +1,88 @@
 package com.devcode.colordetection
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.devcode.colordetection.databinding.ActivityMainBinding
+import org.opencv.android.CameraActivity
+import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.OpenCVLoader
+import org.opencv.core.Mat
+import java.util.Collections
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : CameraActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var cameraBridgeViewBase: CameraBridgeViewBase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (OpenCVLoader.initDebug()) {
-            Toast.makeText(this, "OpenCV is loaded", Toast.LENGTH_SHORT).show()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getPermission()
+
+        val camera = binding.cameraView
+        cameraBridgeViewBase = camera
+        cameraBridgeViewBase.setCvCameraViewListener(object :
+            CameraBridgeViewBase.CvCameraViewListener2 {
+            override fun onCameraViewStarted(width: Int, height: Int) {}
+
+            override fun onCameraViewStopped() {}
+
+            override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame): Mat {
+                return inputFrame.rgba()
+            }
+        })
+        if (OpenCVLoader.initLocal()) {
+            Toast.makeText(this, "OpenCV loaded", Toast.LENGTH_SHORT).show()
+            cameraBridgeViewBase.enableView()
         } else {
-            Toast.makeText(this, "OpenCV is not loaded", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "OpenCV not loaded", Toast.LENGTH_SHORT).show()
         }
+
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        cameraBridgeViewBase.enableView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraBridgeViewBase.disableView()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        cameraBridgeViewBase.disableView()
+    }
+
+    override fun getCameraViewList(): MutableList<out CameraBridgeViewBase> {
+        return Collections.singletonList(cameraBridgeViewBase)
+    }
+
+    private fun getPermission() {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, 101)
+            }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            getPermission()
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 }
---
+++ openCV/build/intermediates/cxx/Debug/4u3w1h17/logs/arm64-v8a/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\alvin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HF:\\Project_Mobile\\Kotlin\\Ml_Kit\\ColorDetection\\openCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=22" ^
+  "-DANDROID_PLATFORM=android-22" ^
+  "-DANDROID_ABI=arm64-v8a" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=arm64-v8a" ^
+  "-DANDROID_NDK=C:\\Users\\alvin\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\alvin\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\alvin\\AppData\\Local\\Android\\Sdk\\ndk\\25.1.8937393\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\alvin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=F:\\Project_Mobile\\Kotlin\\Ml_Kit\\ColorDetection\\openCV\\build\\intermediates\\cxx\\Debug\\4u3w1h17\\obj\\arm64-v8a" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=F:\\Project_Mobile\\Kotlin\\Ml_Kit\\ColorDetection\\openCV\\build\\intermediates\\cxx\\Debug\\4u3w1h17\\obj\\arm64-v8a" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BF:\\Project_Mobile\\Kotlin\\Ml_Kit\\ColorDetection\\openCV\\.cxx\\Debug\\4u3w1h17\\arm64-v8a" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
