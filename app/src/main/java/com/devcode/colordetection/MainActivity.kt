7ddb18a49cfd04c8c8473b421154e967dde2cf76
AlvinPradanaAntony <quickgaming321@gmail.com>
2024-05-24 22:11:54 +0700

    Fix error resolution list

--- app/src/main/java/com/devcode/colordetection/MainActivity.kt
+++ app/src/main/java/com/devcode/colordetection/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.devcode.colordetection
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.pm.PackageManager
 import android.hardware.Camera
 import android.os.Bundle
@@ -8,7 +9,6 @@ import android.util.Log
 import android.view.View
 import android.view.WindowManager
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,24 +18,18 @@ import org.opencv.android.CameraActivity
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.JavaCameraView
 import org.opencv.android.OpenCVLoader
-import org.opencv.core.Core
 import org.opencv.core.CvType
 import org.opencv.core.Mat
-import org.opencv.core.MatOfPoint
-import org.opencv.core.Scalar
-import org.opencv.imgproc.Imgproc
-import weka.classifiers.Classifier
-import weka.core.Attribute
-import weka.core.DenseInstance
-import weka.core.Instances
-import weka.core.SerializationHelper
 import java.util.Collections
+
 
 class MainActivity : CameraActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var cameraBridgeViewBase: CameraBridgeViewBase
     private lateinit var camera: JavaCameraView
     private lateinit var rvList: RecyclerView
+    private lateinit var alertDialog: AlertDialog.Builder
+    private lateinit var dialog: AlertDialog
     private lateinit var mCamera: Camera
     private lateinit var mRgba: Mat
     private lateinit var mGray: Mat
@@ -49,18 +43,17 @@ class MainActivity : CameraActivity() {
     var liveC: String? = null
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContentView(binding.root)
-        setupView()
         setupCamera()
         setupAction()
-    }
-
-    private fun setupView(){
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-        list.addAll(resolutionList())
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            list.addAll(resolutionList())
+        }
     }
 
     private fun setupCamera() {
@@ -74,7 +67,7 @@ class MainActivity : CameraActivity() {
                 mHsv = Mat(width, height, CvType.CV_8UC3)
                 mRgbaT = Mat(height, width, CvType.CV_8UC4)
                 mRgbaF = Mat(fh, fw, CvType.CV_8UC4)
-                Log.w("Resolution", "Width: $width, Height: $height")
+                Log.w("Camera View Size", "Width: $width, Height: $height")
             }
 
             override fun onCameraViewStopped() {
@@ -85,47 +78,47 @@ class MainActivity : CameraActivity() {
                 mRgba = inputFrame.rgba()
                 mGray = inputFrame.gray()
 
-                val corners = MatOfPoint()
-                Imgproc.goodFeaturesToTrack(mGray, corners, 10, 0.5, 20.0, Mat(), 2, false)
-                //Point[] cornersArr = corners.toArray();
-                //Point[] cornersArr = corners.toArray();
-                var R: Int
-                var G: Int
-                var B: Int
-
-                //convert mat rgb to mat hsv-----------------*/
-                Imgproc.cvtColor(mRgba, mHsv, Imgproc.COLOR_RGB2HSV)
-
-                //find scalar sum of (array elements) hsv
-                val mColorHsv = Core.sumElems(mHsv)
-
-                //int pointCount = 320*240; //9:12
-                //int pointCount = 540*720; //more accurate
-                val pointCount = 600 * 800 //4:3
-
-                //convert each pixel
-                for (i in mColorHsv.`val`.indices) {
-                    mColorHsv.`val`[i] /= pointCount.toDouble()
-                }
-
-
-                //convert hsv scalar to rgb scalar
-                val mColorRgb: Scalar = convertScalarHsv2Rgba(mColorHsv)
-                Log.d("intensity", "Color: #${String.format("%02X", mColorHsv.`val`[0].toInt())}${String.format("%02X", mColorHsv.`val`[1].toInt())}${String.format("%02X", mColorHsv.`val`[2].toInt())}")
-
-                // Log.d("intensity", "Color: #" + String.format("%02X", (int) mColorHsv.val[0])+ String.format("%02X", (int) mColorHsv.val[1])+ String.format("%02X", (int) mColorHsv.val[2]));
-                //Get RGB Values
-                R = mColorRgb.`val`[0].toInt()
-                G = mColorRgb.`val`[1].toInt()
-                B = mColorRgb.`val`[2].toInt()
-                val modelAndHeaderEN = arrayOf<Array<Any>?>(null)
-                try {
-                    modelAndHeaderEN[0] = SerializationHelper.readAll(assets.open("l2_rgb_en_k-1.model"))
-                } catch (e: java.lang.Exception) {
-                    e.printStackTrace()
-                }
-                liveC = knnProcess1(R, G, B, modelAndHeaderEN)
-                Log.d("Color Result", "Color: $liveC")
+//                val corners = MatOfPoint()
+//                Imgproc.goodFeaturesToTrack(mGray, corners, 10, 0.5, 20.0, Mat(), 2, false)
+//                //Point[] cornersArr = corners.toArray();
+//                //Point[] cornersArr = corners.toArray();
+//                var R: Int
+//                var G: Int
+//                var B: Int
+//
+//                //convert mat rgb to mat hsv-----------------*/
+//                Imgproc.cvtColor(mRgba, mHsv, Imgproc.COLOR_RGB2HSV)
+//
+//                //find scalar sum of (array elements) hsv
+//                val mColorHsv = Core.sumElems(mHsv)
+//
+//                //int pointCount = 320*240; //9:12
+//                //int pointCount = 540*720; //more accurate
+//                val pointCount = 600 * 800 //4:3
+//
+//                //convert each pixel
+//                for (i in mColorHsv.`val`.indices) {
+//                    mColorHsv.`val`[i] /= pointCount.toDouble()
+//                }
+//
+//
+//                //convert hsv scalar to rgb scalar
+//                val mColorRgb: Scalar = convertScalarHsv2Rgba(mColorHsv)
+//                Log.d("intensity", "Color: #${String.format("%02X", mColorHsv.`val`[0].toInt())}${String.format("%02X", mColorHsv.`val`[1].toInt())}${String.format("%02X", mColorHsv.`val`[2].toInt())}")
+//
+//                // Log.d("intensity", "Color: #" + String.format("%02X", (int) mColorHsv.val[0])+ String.format("%02X", (int) mColorHsv.val[1])+ String.format("%02X", (int) mColorHsv.val[2]));
+//                //Get RGB Values
+//                R = mColorRgb.`val`[0].toInt()
+//                G = mColorRgb.`val`[1].toInt()
+//                B = mColorRgb.`val`[2].toInt()
+//                val modelAndHeaderEN = arrayOf<Array<Any>?>(null)
+//                try {
+//                    modelAndHeaderEN[0] = SerializationHelper.readAll(assets.open("l2_rgb_en_k-1.model"))
+//                } catch (e: java.lang.Exception) {
+//                    e.printStackTrace()
+//                }
+//                liveC = knnProcess1(R, G, B, modelAndHeaderEN)
+//                Log.d("Color Result", "Color: $liveC")
                 return mRgba
             }
         })
@@ -158,9 +151,8 @@ class MainActivity : CameraActivity() {
             }
         }
         binding.btnResolutions.setOnClickListener {
-            val alertDialog = AlertDialog.Builder(this@MainActivity)
-            val dialog = alertDialog.create()
-
+            alertDialog = AlertDialog.Builder(this@MainActivity)
+            dialog = alertDialog.create()
             val dialogView = layoutInflater.inflate(R.layout.item_layout_info_dialog, null, false)
             dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
             dialog.setView(dialogView)
@@ -176,24 +168,29 @@ class MainActivity : CameraActivity() {
     }
 
     private fun resolutionList(): ArrayList<ListRes>{
-        mCamera= Camera.open()
-        val parameters = mCamera.parameters
-        val sizes: List<Camera.Size> = parameters.supportedPreviewSizes
-        val resolution_array_list = ArrayList<ListRes>()
+        try {
+            mCamera= Camera.open()
+            val parameters = mCamera.parameters
+            val sizes: List<Camera.Size> = parameters.supportedPreviewSizes
+            val resolution_array_list = ArrayList<ListRes>()
 
-        for (i in sizes.indices) {
-            try {
-                val frameWidth = sizes[i].width
-                val frameHeight = sizes[i].height
-                val res = "$frameWidth x $frameHeight"
-                val listRes = ListRes(res, frameWidth, frameHeight)
-                resolution_array_list.add(listRes)
-                Log.w("Resolution", "Resolution: $res")
-            } catch (e: Exception) {
-                Log.e("Error Resolution", "Error: ${e.message}")
+            for (i in sizes.indices) {
+                try {
+                    val frameWidth = sizes[i].width
+                    val frameHeight = sizes[i].height
+                    val res = "$frameWidth x $frameHeight"
+                    val listRes = ListRes(res, frameWidth, frameHeight)
+                    resolution_array_list.add(listRes)
+                    Log.w("Resolution List", "Resolution: $res")
+                } catch (e: Exception) {
+                    Log.e("Error Resolution List", "Error: ${e.message}")
+                }
             }
+            return resolution_array_list
+        } catch (e: Exception) {
+            Log.e("Error Camera", "Error: ${e.message}")
         }
-        return resolution_array_list
+        return ArrayList()
     }
 
     private fun showRecyclerList() {
@@ -211,16 +208,17 @@ class MainActivity : CameraActivity() {
                 camera.setMaxFrameSize(frameWidth, frameHeight)
                 camera.enableView()
                 Toast.makeText(this@MainActivity, "$resolution, Width: $frameWidth, Height: $frameHeight", Toast.LENGTH_SHORT).show()
+                dialog.dismiss()
             }
         })
     }
 
-    private fun convertScalarHsv2Rgba(hsvColor: Scalar): Scalar {
-        val pointMatRgba = Mat()
-        val pointMatHsv = Mat(1, 1, CvType.CV_8UC3, hsvColor)
-        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB)
-        return Scalar(pointMatRgba[0, 0])
-    }
+//    private fun convertScalarHsv2Rgba(hsvColor: Scalar): Scalar {
+//        val pointMatRgba = Mat()
+//        val pointMatHsv = Mat(1, 1, CvType.CV_8UC3, hsvColor)
+//        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB)
+//        return Scalar(pointMatRgba[0, 0])
+//    }
 
 //    private fun knnProcess1(r: Int, g: Int, b: Int, modelAndHeader: Array<Array<Any>?>): String? {
 //        var prediction: String? = null
@@ -276,51 +274,51 @@ class MainActivity : CameraActivity() {
 //        return prediction
 //    }
 
-    private fun knnProcess1(r: Int, g: Int, b: Int, modelAndHeader: Array<Array<Any>?>): String? {
-        var prediction: String? = null
-        try {
-            // Create list of attributes.
-            val numericAtt = ArrayList<Attribute>()
-            val nominalAtt = ArrayList<String>()
-
-            // Add numeric attributes/columns
-            numericAtt.add(Attribute("R"))
-            numericAtt.add(Attribute("G"))
-            numericAtt.add(Attribute("B"))
-
-            // Add nominal/letters attributes/column
-            nominalAtt.add("val3") // label for the nominal attribute
-            numericAtt.add(Attribute("Color Names", nominalAtt))
-
-            // Create Instances object
-            val data = Instances("Testing Data", numericAtt, 1000)
-
-            // Fill with data
-            val vals = doubleArrayOf(r.toDouble(), g.toDouble(), b.toDouble(), nominalAtt.indexOf("val3").toDouble())
-
-            // Add to instance
-            data.add(DenseInstance(1.0, vals))
-            data.setClassIndex(data.numAttributes() - 1)
-
-            // KNN Weka part
-            val modelAndHeaderElement = modelAndHeader[0]
-            if (modelAndHeaderElement?.size != 2) {
-                throw Exception("[InputMappedClassifier] serialized model file does not seem to contain both a model and the instances header used in training it!")
-            } else {
-                val knnmodel = modelAndHeaderElement[0] as Classifier
-                val m_modelHeader = modelAndHeaderElement[1] as Instances
-
-                val value = knnmodel.classifyInstance(data.instance(0))
-
-                // Get the name of the class value
-                prediction = m_modelHeader.classAttribute().value(value.toInt())
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return prediction
-    }
+//    private fun knnProcess1(r: Int, g: Int, b: Int, modelAndHeader: Array<Array<Any>?>): String? {
+//        var prediction: String? = null
+//        try {
+//            // Create list of attributes.
+//            val numericAtt = ArrayList<Attribute>()
+//            val nominalAtt = ArrayList<String>()
+//
+//            // Add numeric attributes/columns
+//            numericAtt.add(Attribute("R"))
+//            numericAtt.add(Attribute("G"))
+//            numericAtt.add(Attribute("B"))
+//
+//            // Add nominal/letters attributes/column
+//            nominalAtt.add("val3") // label for the nominal attribute
+//            numericAtt.add(Attribute("Color Names", nominalAtt))
+//
+//            // Create Instances object
+//            val data = Instances("Testing Data", numericAtt, 1000)
+//
+//            // Fill with data
+//            val vals = doubleArrayOf(r.toDouble(), g.toDouble(), b.toDouble(), nominalAtt.indexOf("val3").toDouble())
+//
+//            // Add to instance
+//            data.add(DenseInstance(1.0, vals))
+//            data.setClassIndex(data.numAttributes() - 1)
+//
+//            // KNN Weka part
+//            val modelAndHeaderElement = modelAndHeader[0]
+//            if (modelAndHeaderElement?.size != 2) {
+//                throw Exception("[InputMappedClassifier] serialized model file does not seem to contain both a model and the instances header used in training it!")
+//            } else {
+//                val knnmodel = modelAndHeaderElement[0] as Classifier
+//                val m_modelHeader = modelAndHeaderElement[1] as Instances
+//
+//                val value = knnmodel.classifyInstance(data.instance(0))
+//
+//                // Get the name of the class value
+//                prediction = m_modelHeader.classAttribute().value(value.toInt())
+//            }
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//
+//        return prediction
+//    }
 
     public override fun onResume() {
         super.onResume()
@@ -347,6 +345,8 @@ class MainActivity : CameraActivity() {
     private fun getPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, 101)
+        } else{
+            list.addAll(resolutionList())
         }
     }
 
@@ -355,6 +355,8 @@ class MainActivity : CameraActivity() {
 
         if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
             getPermission()
+        } else {
+            list.addAll(resolutionList())
         }
     }
 
---
+++ openCV/build/intermediates/cxx/Debug/4u3w1h17/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\alvin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "F:\\Project_Mobile\\Kotlin\\Ml_Kit\\ColorDetection\\openCV\\.cxx\\Debug\\4u3w1h17\\arm64-v8a" ^
+  opencv_jni_shared
